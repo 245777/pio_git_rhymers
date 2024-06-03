85524396a1dadd097e20d67973aaea2042c0110e
Jan Boruń <245777@edu.p.lodz.pl>
2024-06-03 11:35:13 +0200

    dzial 2, zad 11 - hermetyzacja nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,39 +4,58 @@ public class DefaultCountingOutRhymer {
 
     private static final int empty = -1;
     private static final int capacity = 12;
-    private static final int full = capacity-1;
-    private final int[] numbers = new int[capacity];
+    private static final int full = getCapacity() -1;
+    private final int[] numbers = new int[getCapacity()];
+
+    public static int getEmpty() {
+        return empty;
+    }
+
+    public static int getCapacity() {
+        return capacity;
+    }
+
+    public static int getFull() {
+        return full;
+    }
 
     public int getTotal() {
         return total;
     }
 
-    private int total = empty;
+    private int total = getEmpty();
 
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            getNumbers()[setTotal(getTotal() + 1)] = in;
     }
 
         public boolean callCheck() {
-            return total == empty;
+            return getTotal() == getEmpty();
         }
         
             public boolean isFull() {
-                return total == full;
+                return getTotal() == getFull();
             }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return empty;;
-                    return numbers[total];
+                        return getEmpty();;
+                    return getNumbers()[getTotal()];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return empty;
-                        return numbers[total--];
+                            return getEmpty();
+                        return getNumbers()[setTotal(getTotal() - 1)];
                     }
 
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
